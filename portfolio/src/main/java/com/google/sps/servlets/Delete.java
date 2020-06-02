0cbf3180c693c0cc2d83d74c4c37691da11f0cfe
Maryam <amaryam294@gmail.com>
2020-06-02 17:29:00 +0000

    Fix delete issue.

---
+++ portfolio/src/main/java/com/google/sps/servlets/Delete.java
@@ -0,0 +1,43 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.KeyRange;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import com.google.gson.Gson;
+
+// Deletes all the comment data on the page
+@WebServlet("/delete-data")
+public class Delete extends HttpServlet {
+
+  @Override
+  // Get text input from comment form and respond with result
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Obtain comments from data store and filter them into results query
+    Query query = new Query("Visitor");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    // Load the comments into the list of messages
+    for (Entity entity : results.asIterable()) {
+      Key id = entity.getKey();
+      datastore.delete(id);
+    }
+  }
+}
